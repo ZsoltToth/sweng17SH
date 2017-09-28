dbab03cf55d6ccd49715a05e92ed45e5cdf0b45f
Zsolt Toth <tothzs87@gmail.com>
2017-09-28 11:04:12 +0200

    ActorTest has been added but it is empty. App and AppTest generated files are removed.

---
+++ cineval/core/src/test/java/hu/uni/miskolc/iit/sweng/core/model/ActorTest.java
@@ -0,0 +1,16 @@
+package hu.uni.miskolc.iit.sweng.core.model;
+
+
+import org.junit.Test;
+import static  org.junit.Assert.*;
+
+/**
+ * Created by tothzs on 2017.09.28..
+ */
+public class ActorTest {
+
+    @Test
+    public void test(){
+        fail("hello world");
+    }
+}
